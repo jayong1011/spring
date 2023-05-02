373610394a0525aca1a6d305d2bde746716ef0bb
jayong1011 <20181612@edu.hanbat.ac.kr>
2023-05-02 15:59:21 +0900

    kim

--- study/src/main/java/jayong/study/controller/HelloController.java
+++ study/src/main/java/jayong/study/controller/HelloController.java
@@ -1,2 +1,18 @@
-package jayong.study.controller;public class HelloController {
+package jayong.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+
 }
